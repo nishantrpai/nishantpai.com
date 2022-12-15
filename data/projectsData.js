34489b56c58c3eef0795ac4667d72074ea828def
nishantrpai <nishantrpai@gmail.com>
2022-12-15 14:11:10 +0530

    add project links

--- data/projectsData.js
+++ data/projectsData.js
@@ -23,42 +23,42 @@ const projectsData = [
     imgSrc: 'https://0xverify.app/0xverify_discord.png',
     href: 'https://0xverify.com/',
   },
-  {
-    title: 'Privacy Santa (Sold)',
-    description: `Grow your business online, we'll manage your privacy policy`,
-    imgSrc: 'https://privacysanta.com/images/preview.jpg',
-    href: 'https://privacysanta.com',
-  },
-  {
-    title: 'Logify',
-    description: `Use Logify for keeping track of context across projects and timestamping events.`,
-    imgSrc: 'https://logify.vercel.app/logify_og_image.png',
-    href: 'https://logify.vercel.app/',
-  },
-  {
-    title: 'Threadify',
-    description: `Turn your blog into tweets`,
-    imgSrc: 'https://i.imgur.com/SXjZTze.png',
-    href: 'https://threadify.vercel.app/',
-  },
-  {
-    title: 'Covid19 Status',
-    description: `Track the current status of the COVID-19 and stay up to date about Novel Coronavirus in your location and what you can do to prevent getting infected or spreading of Coronavirus`,
-    imgSrc: 'https://i.imgur.com/BRgRtmi.png',
-    href: 'https://literate-octo-journey.vercel.app/',
-  },
-  {
-    title: 'Youtube Thumbnail',
-    description: `Generate thumbnail for your Youtube video`,
-    imgSrc: 'https://i.imgur.com/OtXHhxD.png',
-    href: 'https://ythumbnail.vercel.app/',
-  },
-  {
-    title: 'Visualizevalue',
-    description: `Animates visuals from visualizevalue`,
-    imgSrc: 'https://i.imgur.com/4vYjJUE.png',
-    href: 'https://visualizevalue.vercel.app/',
-  },
+  // {
+  //   title: 'Privacy Santa (Sold)',
+  //   description: `Grow your business online, we'll manage your privacy policy`,
+  //   imgSrc: 'https://privacysanta.com/images/preview.jpg',
+  //   href: 'https://privacysanta.com',
+  // },
+  // {
+  //   title: 'Logify',
+  //   description: `Use Logify for keeping track of context across projects and timestamping events.`,
+  //   imgSrc: 'https://logify.vercel.app/logify_og_image.png',
+  //   href: 'https://logify.vercel.app/',
+  // },
+  // {
+  //   title: 'Threadify',
+  //   description: `Turn your blog into tweets`,
+  //   imgSrc: 'https://i.imgur.com/SXjZTze.png',
+  //   href: 'https://threadify.vercel.app/',
+  // },
+  // {
+  //   title: 'Covid19 Status',
+  //   description: `Track the current status of the COVID-19 and stay up to date about Novel Coronavirus in your location and what you can do to prevent getting infected or spreading of Coronavirus`,
+  //   imgSrc: 'https://i.imgur.com/BRgRtmi.png',
+  //   href: 'https://literate-octo-journey.vercel.app/',
+  // },
+  // {
+  //   title: 'Youtube Thumbnail',
+  //   description: `Generate thumbnail for your Youtube video`,
+  //   imgSrc: 'https://i.imgur.com/OtXHhxD.png',
+  //   href: 'https://ythumbnail.vercel.app/',
+  // },
+  // {
+  //   title: 'Visualizevalue',
+  //   description: `Animates visuals from visualizevalue`,
+  //   imgSrc: 'https://i.imgur.com/4vYjJUE.png',
+  //   href: 'https://visualizevalue.vercel.app/',
+  // },
 ]
 
 export default projectsData
