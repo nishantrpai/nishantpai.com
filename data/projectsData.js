e5ae3473c99c9e8e44d197e0b43b892145e5723f
nishantrpai <nishantrpai@gmail.com>
2023-06-06 17:02:47 +0530

    change projects to links

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,15 +1,26 @@
 const projectsData = [
   {
-    title: '0xwall',
-    description: `Web3 Paywall any link you want with few clicks. No KYC, no deposit, no documents, no charges on any transaction.`,
-    imgSrc: 'https://0xwall.app/og.png',
-    href: 'https://0xwall.app/',
-  },
-  {
     title: 'GM Bot',
     description: `A Discord bot that reacts anytime someone says "GM". Includes features like customizing the reaction, and more.`,
     imgSrc: 'https://addgmbot.com/og.png',
     href: 'https://addgmbot.com/',
+  },
+  {
+    title: 'focus',
+    description: `An app to help you achieve your goals`,
+    imgSrc: 'https://focus.app/og.png',
+    href: 'https://focus.app/',
+  },
+  {
+    title: 'Alertbuzz',
+    description: `Uncover valuable leads seeking your offerings with our Telegram bot delivering real-time posts from Reddit, Twitter, and Hacker News`,
+    imgSrc: 'https://alert.buzz/og.png',
+  },
+  {
+    title: '0xwall',
+    description: `Web3 Paywall any link you want with few clicks. No KYC, no deposit, no documents, no charges on any transaction.`,
+    imgSrc: 'https://0xwall.app/og.png',
+    href: 'https://0xwall.app/',
   },
   {
     title: '0xverify',
@@ -37,42 +48,6 @@ const projectsData = [
       'https://opengraph.githubassets.com/082e76b287c2665d97f1b38ac5e04d4230080848f770967c1e54c41e87ff7374/nishantrpai/rarity-score',
     href: 'https://github.com/nishantrpai/rarity-score',
   },
-  // {
-  //   title: 'Privacy Santa (Sold)',
-  //   description: `Grow your business online, we'll manage your privacy policy`,
-  //   imgSrc: 'https://privacysanta.com/images/preview.jpg',
-  //   href: 'https://privacysanta.com',
-  // },
-  // {
-  //   title: 'Logify',
-  //   description: `Use Logify for keeping track of context across projects and timestamping events.`,
-  //   imgSrc: 'https://logify.vercel.app/logify_og_image.png',
-  //   href: 'https://logify.vercel.app/',
-  // },
-  // {
-  //   title: 'Threadify',
-  //   description: `Turn your blog into tweets`,
-  //   imgSrc: 'https://i.imgur.com/SXjZTze.png',
-  //   href: 'https://threadify.vercel.app/',
-  // },
-  // {
-  //   title: 'Covid19 Status',
-  //   description: `Track the current status of the COVID-19 and stay up to date about Novel Coronavirus in your location and what you can do to prevent getting infected or spreading of Coronavirus`,
-  //   imgSrc: 'https://i.imgur.com/BRgRtmi.png',
-  //   href: 'https://literate-octo-journey.vercel.app/',
-  // },
-  // {
-  //   title: 'Youtube Thumbnail',
-  //   description: `Generate thumbnail for your Youtube video`,
-  //   imgSrc: 'https://i.imgur.com/OtXHhxD.png',
-  //   href: 'https://ythumbnail.vercel.app/',
-  // },
-  // {
-  //   title: 'Visualizevalue',
-  //   description: `Animates visuals from visualizevalue`,
-  //   imgSrc: 'https://i.imgur.com/4vYjJUE.png',
-  //   href: 'https://visualizevalue.vercel.app/',
-  // },
 ]
 
 export default projectsData
