dcbda7f27813348ade507f80c882aa6f888bdedf
nishantrpai <nishantrpai@gmail.com>
2023-06-06 17:09:48 +0530

    update projects

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,7 +1,7 @@
 const headerNavLinks = [
   { href: '/blog', title: '📜 Blog' },
   { href: '/tags', title: '🏷️ Tags' },
-  { href: '/projects', title: '📦 Projects' },
+  { href: '/links', title: '📦 Links' },
 ]
 
 export default headerNavLinks
