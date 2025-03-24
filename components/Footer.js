f280d7b174dc8d427ba399a97e7781e6c27c2642
nishu <nishantrpai@gmail.com>
2025-03-24 18:51:28 +0530

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -7,12 +7,12 @@ export default function Footer() {
     <footer>
       <div className="flex flex-col items-center mt-16">
         <div className="flex mb-3 space-x-4">
-          <SocialIcon kind="mail" href={`mailto:nishantrpai@gmail.com`} size="10" />
-          <SocialIcon kind="github" href={siteMetadata.github} size="10" />
-          <SocialIcon kind="facebook" href={siteMetadata.facebook} size="10" />
-          <SocialIcon kind="youtube" href={siteMetadata.youtube} size="10" />
-          <SocialIcon kind="linkedin" href={siteMetadata.linkedin} size="10" />
-          <SocialIcon kind="twitter" href={siteMetadata.twitter} size="10" />
+          <SocialIcon kind="mail" href={`mailto:nishantrpai@gmail.com`} size="8" />
+          <SocialIcon kind="github" href={siteMetadata.github} size="8" />
+          <SocialIcon kind="facebook" href={siteMetadata.facebook} size="8" />
+          <SocialIcon kind="youtube" href={siteMetadata.youtube} size="8" />
+          <SocialIcon kind="linkedin" href={siteMetadata.linkedin} size="8" />
+          <SocialIcon kind="twitter" href={siteMetadata.twitter} size="8" />
         </div>
         <div className="flex mb-2 space-x-2 text-sm text-gray-500 dark:text-gray-400">
           <div>{siteMetadata.author}</div>
